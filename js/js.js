ac7125f577fa46b46cbd2d7beedb5392e3d2bd2c
nnedqtc <nikolai.nedqlkov00@gmail.com>
2018-06-17 16:15:24 +0300

    second commit

--- js/js.js
+++ js/js.js
@@ -1,1 +1,8 @@
-window.onload(alert('You have to be above the age of 18'));
+window.onload = function (){ Confirm(); }
+function Confirm()
+{
+    var answer = confirm ("Are you above the age of 18?")
+    if (!answer)
+    window.location="http://www.comsed.net"
+}
+
