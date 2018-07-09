f2efc41175009444aa07d53baa9a6791c588e7cf
nnedqtc <nikolai.nedqlkov00@gmail.com>
2018-07-09 11:31:31 +0300

    added few jquery functions

--- js/jsnothome.js
+++ js/jsnothome.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
     $('.ex1').zoom();
 });
+
+
+$(document).ready(function(){
+    $(".main-h1").hover(function(){
+        $(".main-h2").fadeIn(1500);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".example").click(function(){
+        $(".toggle").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+
+$(document).ready(function(){
+    $(".example1").click(function(){
+        $(".toggle1").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $(".example2").click(function(){
+        $(".toggle2").animate({
+            height: 'toggle'
+        });
+    });
+});
