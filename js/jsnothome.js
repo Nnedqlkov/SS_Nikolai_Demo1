806d9143e892a7aca67957dcfd48c496f58844b9
nnedqtc <nikolai.nedqlkov00@gmail.com>
2018-07-10 23:53:43 +0300

    added some jquery functions

--- js/jsnothome.js
+++ js/jsnothome.js
@@ -34,3 +34,99 @@ $(document).ready(function(){
         });
     });
 });
+
+$(document).ready(function(){
+    $("#tt1").click(function(){
+        $("#tooltip1").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#tt2").click(function(){
+        $("#tooltip2").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#tt3").click(function(){
+        $("#tooltip3").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+
+$(document).ready(function(){
+    $("#tt4").click(function(){
+        $("#tooltip4").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+
+$(document).ready(function(){
+    $("#tt5").click(function(){
+        $("#tooltip5").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+
+$(document).ready(function(){
+    $("#tt6").click(function(){
+        $("#tooltip6").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+
+$(document).ready(function(){
+    $("#tt7").click(function(){
+        $("#tooltip7").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+
+$(document).ready(function(){
+    $("#tt8").click(function(){
+        $("#tooltip8").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+
+$(document).ready(function(){
+    $("#tt9").click(function(){
+        $("#tooltip9").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+
+$(document).ready(function(){
+    $("#tt10").click(function(){
+        $("#tooltip10").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+
+$(document).ready(function(){
+    $("#tt11").click(function(){
+        $("#tooltip11").animate({
+            height: 'toggle'
+        });
+    });
+});
